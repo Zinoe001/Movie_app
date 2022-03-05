a8f2c1d0e2b7ee6541dd06e4f9f49c27b85c37a1
Zinoe001 <zinoetech@gmail.com>
2022-03-05 16:34:45 +0100

    added primary color

---
+++ lib/utils/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> _primaryColor = {
+  50: const Color(0XFF9400D3).withOpacity(0.1),
+  100: const Color(0XFF9400D3).withOpacity(0.2),
+  200: const Color(0XFF9400D3).withOpacity(0.3),
+  300: const Color(0XFF9400D3).withOpacity(0.4),
+  400: const Color(0XFF9400D3).withOpacity(0.5),
+  500: const Color(0XFF9400D3).withOpacity(0.6),
+  600: const Color(0XFF9400D3).withOpacity(0.7),
+  700: const Color(0XFF9400D3).withOpacity(0.8),
+  800: const Color(0XFF9400D3).withOpacity(0.9),
+  900: const Color(0XFF9400D3),
+};
+final MaterialColor kPrimariyColor = MaterialColor((0XFF9400D3), _primaryColor);

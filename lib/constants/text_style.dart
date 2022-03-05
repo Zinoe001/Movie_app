9ea1221a99fb9e4cdcffcfc39afa6c5b5db56d8c
Zinoe001 <zinoetech@gmail.com>
2022-03-05 16:11:34 +0100

    Initial Setup, added textStyles

---
+++ lib/constants/text_style.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle headingStyle1 = GoogleFonts.barlow(
+  fontSize: 32,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle2 = GoogleFonts.barlow(
+  fontSize: 28,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle3 = GoogleFonts.barlow(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle4 = GoogleFonts.barlow(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle5 = GoogleFonts.barlow(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle6 = GoogleFonts.barlow(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle bodyStyle1 = GoogleFonts.barlow(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle bodyStyle2 = GoogleFonts.barlow(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle captionStyle = GoogleFonts.barlow(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+);
